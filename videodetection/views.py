782d812609c32d271b094170640fba02ab69b1bf
Govinda Mandal <govinda4india@gmail.com>
2024-03-03 17:44:36 +0530

    Variable bug fixed

--- videodetection/views.py
+++ videodetection/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     context = {
-      fullName: "Shubham Kshariya",
-      age: 22,
-      destination: "Software Engineer"
+      'fullName': "Shubham Gupta",
+      'age': 22,
+      'destination': "Software Engineer"
     }
     return render(request, 'index.html', context)
